6ba8854fa72f2f8bc9ee12e87b4df8b96f79fd5a
Isack Nkolima <106377487+MrNkolima@users.noreply.github.com>
2023-02-28 14:29:29 +0300

    Add files via upload
    Two files one is client-side.py and the other is server-side.py

---
+++ client-side.py
@@ -0,0 +1,157 @@
+import socket
+import sys
+import time
+import os
+
+def text_out(text: str) -> None:
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+host=input("Write a Server address:: ")
+if host=="":
+   host="localhost"
+   text_out("Default:: localhost")
+   print("")
+else:
+   pass
+
+port=5050
+bufferSize=1048576
+
+
+client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host,port))
+
+
+
+
+class file:
+    def __init__(self,x,y):
+       self.x,self.y=x,y
+    def compress(contents,filename):
+       message=contents+"<_SEPARATOR_>"+filename
+       return message   
+       
+    def findExtension(file):
+        # This function gonna return the filename(base) of the file,
+        # From the link provided other wise return the filename
+        if "/" in file:
+            splitted=file.split("/")
+            filename=splitted[-1:]
+            return filename[0]
+        else:
+            filename=file
+            return filename    
+            
+    def openFile(file_dir,ext):
+       extAll=ext.split(".")
+       extension=extAll[1]
+       all="jpg jpeg gif png JPEG tiff mp4 mp3 dox ppx avi pdf wav ogg zip bin csv exe ico svg bat dat sql tar gz "
+       # Here we'll deal with those non-text files
+       if extension in all:
+           return ''
+       # Here we'll deal with the text natured files
+       else:
+           f=open(file_dir,"rt")
+           contents=f.read()
+       return contents
+
+    def separate(data):
+        # Spliting the data received as content and filename
+        texts=data.split("<_SEPARATOR_>")
+        datas=texts[0]    
+        ext=texts[1]
+        return datas,ext
+ 
+ 
+ 
+ 
+os.system("clear")
+print("\n\n")
+text_out("        CHAT WITH THE SERVER NOW !!!! ")
+print("")
+
+
+
+# Main loop
+while True:
+  try:
+    # SENDING 
+    message=input("MESSAGE TO SERVER:: ")
+    message=f'''{message}'''
+    if(message=="file"):
+     # When user wants to send the files
+     try:
+          file_dir=input("         [File-Name] e.g(./file.py) :: ")
+          # Finding the file name extensiom
+          extension=file.findExtension(file_dir)
+          # Here we read the file
+          contents=file.openFile(file_dir,extension)
+          
+          # This make sure that the files which are being transfered are,
+          # Text based and not binaries.
+          # In the future upgrades we'll be able to transfer them.
+          if(contents==""):
+              client.send("STOPPED".encode())
+              text_out("    [ 403 ] :: Binary files are not allowed!!")
+              print("")
+              break 
+          message=file.compress(contents,extension)
+     except:
+          #using this in debug mode
+          client.send("STOPPED".encode())
+          text_out("     [ 400 ] :: No such File or Directory!!")
+          print("")
+          break 
+    client.send(message.encode())
+    
+    
+    # RECEIVING 
+    data=client.recv(bufferSize).decode()
+    text_out("MESSAGE FROM SERVER:: ")
+    # Always convert all responses to strings
+    received=str(data)
+
+    # Receiving segment
+    if(received=="STOPPED"):
+       # If the server response is STOPPED, then abolish all the processes
+       text_out("\n       [ 500 ] :: It seems server is down!!!")
+       print("")
+       break
+    
+    # checks whether received is a file or a normal string
+    compare=int(received.find("<_SEPARATOR_>"))
+    if(compare>=0):
+      contents,filename=file.separate(received)     
+      text_out(f"\n        INCOME FILE DETECTED - {filename} ")
+      print("")
+      response=input(f"     [ x ] :: Do you want to accept {filename}?  [y/n]   ::  ")
+      if response=='y' || response=='Y':
+          file_path=input("     Where should the file be saved? \n     Default is Present Directory:: ")
+          if(file_path==""):
+             os.system(f"echo '{contents}' > {filename}")
+             text_out("    File received at the current directory!!")
+             print("")
+          else:
+             os.system(f"cd {file_path} && echo '{contents}' > {filename}")
+             text_out(f"     {filename} saved! ")
+             print("")
+      else:
+          print("")
+          text_out("     [ 200 ] :: A file successfully deleted!!")
+          print("")
+          break 
+    else:
+      text_out(received)
+      print("")
+  
+  except KeyboardInterrupt:
+   client.send("STOPPED".encode())
+   text_out("   ")
+   print("")
+   break 
+   
+   
+client.close()
---
+++ server-side.py
@@ -0,0 +1,168 @@
+import socket
+import sys
+import time
+import os
+
+# The animation of printing
+def text_out(text: str) -> None:
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.1)
+        
+        
+class file:
+    def separate(data):
+        texts=data.split("<_SEPARATOR_>")
+        datas=texts[0]
+        ext=texts[1]    
+        return datas,ext
+        
+    def compress(contents,filename):
+       message=contents+"<_SEPARATOR_>"+filename
+       return message
+       
+    def findExtension(file):
+        if "/" in file:
+            splitted=file.split("/")
+            filename=splitted[-1:]
+            return filename[0]
+        else:
+            filename=file
+            return filename
+            
+    def openFile(file_dir,ext):
+       extAll=ext.split(".")
+       extension=extAll[1]
+       all="jpg jpeg gif JPEG tiff mp4 mp3 dox ppx avi pdf wav ogg zip bin csv exe ico svg bat dat sql tar gz "
+       if extension in all:
+           return ''
+       else:
+           f=open(file_dir,"rt")\
+           contents=f.read()
+       return contents
+
+
+host=input("Enter Your server Name:: ")
+if host=="":
+    host="localhost"
+    text_out("Default:: localhost")
+    print("")
+else:
+    pass
+
+port=5050
+bufferSize=1048576
+
+
+server=socket.socket()
+server.bind((host,port))
+
+text_out("Waiting  connections from clients...")
+
+server.listen(2)
+conn, addrs=server.accept()
+os.system("clear")
+text_out(f"A server connected to {addrs}")
+print("")
+
+print("\n\n")
+text_out("         WAITING A MESSAGE FROM THE CLIENT !!!")
+print("")
+
+while True:
+  try:
+    # RECEIVING 
+    data=conn.recv(bufferSize).decode()
+    text_out("MESSAGE FROM CLIENT: ")
+    received=str(data)
+    if(received=="STOPPED"):
+       text_out("\n        It seems client has some problem\n         connecting....")
+       print("")
+       break
+    elif type(received) is bytes:
+       print("bit file received")
+       
+    if type(received) is str:
+     print("normal file received")
+     compare=int(received.find("<_SEPARATOR_>"))
+     if(compare>=0):
+      contents,filename=file.separate(received)
+      text_out(f"\n        THE FILE HAS BEEN RECEIVED - {filename} ")
+      print("")
+      file_path=input("     Where should the file be saved? \n     Default is Present Directory:: ")
+      if(file_path==""):
+         try:
+             print(f"Trying openfile...{filename}")
+             f=open(filename,"w")
+             print("File opened (new) but contents not written")
+             f.write(f'''{contents}''')
+             print("File is written but problem is that it didn't close")
+             f.close()
+         except:
+             conn.send("STOPPED".encode())
+             text_out("   Error receiving file from client :: Present Directory!!")
+             print("")
+             break
+         text_out("    File received at the current directory!!")
+         print("")
+      else:
+         full=filepath+filename
+         try:
+             f=open(full,"w")
+             f.write(f'''{contents}''')
+             f.close()
+         except:
+             conn.send("STOPPED".encode())
+             text_out(f"   Error receiving file from client :: at {full}!!")
+             print("")
+             break
+         text_out(f"     {filename} saved! ")
+         print("")
+     else:
+      text_out(received)
+      print("")
+    else:
+      prin  
+      
+      
+    # SENDING 
+    message=input("MESSAGE to CLIENT:: ")
+    message=f'''{message}'''
+    if(message=="quit" or message=="q"):
+        conn.send("STOPPED".encode())
+        text_out("     [ 200 ] :: server shutteddown conditionally!!")
+        print("")
+        break
+    elif(message=="file"):
+       # when wanting to send files
+       try:
+          file_dir=input("         [File-Name]:: ")
+          extension=file.findExtension(file_dir)
+          contents=file.openFile(file_dir,extension)
+          if(contents==''):
+              print('')
+              text_out("     [ 403 ] :: The type of the file you want to send is forbidden!!")
+              print('')
+              conn.send("STOPPED".encode())
+              break()
+          message=file.compress(contents,extension)
+       except:
+          #using this in debug mode
+          conn.send("STOPPED".encode())
+          text_out("No such File or Directory!!")
+          print("")                                        
+          break
+    
+    conn.send(message.encode())
+  except KeyboardInterrupt:
+    conn.send("STOPPED".encode())
+    text_out("  [ 200 ] :: Turning off server...")
+    print("")
+    break  
+    
+    
+conn.close()
+text_out("   [ 200 ] :: The server turned off successfully!!")
+print("")
+server.close()
